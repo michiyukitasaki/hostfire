013de40838158aec93796ff75edf5f92a6b92ef3
田崎倫之 <tasaki1931@gmail.com>
2022-01-24 16:43:59 +0900

    trace complete

---
+++ lib/sections/service/service_section.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:hostfire0123/components/section_title.dart';
+
+import 'package:hostfire0123/constants.dart';
+import 'components/service_card.dart';
+import 'package:hostfire0123/models/Service.dart';
+
+class ServiceSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: kDefaultPadding * 2),
+      constraints: BoxConstraints(maxWidth: 1110),
+      child: Column(
+        children: [
+          SectionTitle(
+            color: Color(0xFFFF0000),
+            title: "Service Offerings",
+            subTitle: "My Strong Arenas",
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: List.generate(
+                services.length, (index) => ServiceCard(index: index)),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/sections/topSection/top_section.dart
@@ -0,0 +1,45 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:hostfire0123/constants.dart';
+import 'package:flutter/services.dart';
+import 'components/logo_blur_box.dart';
+import 'components/menu.dart';
+import 'components/person_pic.dart';
+import 'dart:async';
+
+class TopSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      alignment: Alignment.center,
+      constraints: BoxConstraints(maxHeight: 900, minHeight: 700),
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage("assets/images/background.png"),
+        ),
+      ),
+      child: Container(
+        margin: EdgeInsets.only(top: kDefaultPadding),
+        width: 1200,
+        child: Stack(
+          children: [
+            LogoAndBlurBox(size: size),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: PersonPic(),
+            ),
+            Positioned(
+              bottom: 0,
+              child: Menu(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
